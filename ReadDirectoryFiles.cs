5f1471973d4737e229e84b02496855fe4b37b7e7
Theodoros Kanellopoulos <tkanello@outlook.com>
2021-09-02 17:43:08 +0300

    StartImportFilesFuncionality02.09.2021

---
+++ ReadDirectoryFiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Offers
+{
+    public class ReadDirectoryFiles
+    {
+        public void EnumerateFiles()
+        {
+
+            string sourceDirectory = @"C:\current";
+            string archiveDirectory = @"C:\archive";
+
+            var txtFiles = Directory.EnumerateFiles(sourceDirectory, "*.txt", SearchOption.AllDirectories);
+
+            foreach (string currentFile in txtFiles)
+            {
+                string fileName = currentFile.Substring(sourceDirectory.Length + 1);
+                Directory.Move(currentFile, Path.Combine(archiveDirectory, fileName));
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+}
